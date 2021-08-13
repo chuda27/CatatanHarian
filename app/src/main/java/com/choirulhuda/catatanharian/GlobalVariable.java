bc734f60ef286a5b894f942d0db41fd660eddaf5
Choirul Huda <choirulhuda07@gmail.com>
2021-08-13 17:07:33 +0700

    first commit

---
+++ app/src/main/java/com/choirulhuda/catatanharian/GlobalVariable.java
@@ -0,0 +1,6 @@
+package com.choirulhuda.catatanharian;
+
+public class GlobalVariable {
+    public static String CATATAN_HARIAN = "catatanHarian";
+    public static String LOGIN = "login";
+}
---
+++ app/src/main/java/com/choirulhuda/catatanharian/InsertAndViewActivity.java
@@ -0,0 +1,23 @@
+package com.choirulhuda.catatanharian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class InsertAndViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_and_view);
+
+        setTitle("Catatan Baru");
+    }
+
+    public void actionSumbitNote(View view) {
+        //aksi setelah submit
+        //melakukan koneksi internet
+        //penyimpanan file
+    }
+}
